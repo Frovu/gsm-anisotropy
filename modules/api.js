2e819707ab701f876352820c99236dc25edd29d2
Frovu <sfrovis@gmail.com>
2021-02-03 22:41:09 +0300

    fix: return arrays

--- modules/api.js
+++ modules/api.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('gsm.db', sqlite3.OPEN_READONLY);
 
+const FIELDS = ['time', 'A0', 'Ax', 'Ay', 'Az', 'Axy'];
+
 function select(params) {
 	return new Promise((resolve) => {
 		const from = params.from && parseInt(params.from);
@@ -10,12 +12,13 @@ function select(params) {
 		if(!from || isNaN(from) || !to || isNaN(to))
 			return null;
 		// cursed code here, golang devs would quite dislike it
-		const q = `SELECT * FROM gsm WHERE time >= ${from} AND time < ${to} ORDER BY time`;
+		const q = `SELECT ${FIELDS.join()} FROM gsm WHERE time >= ${from} AND time < ${to} ORDER BY time`;
 		db.all(q, [], (err, rows) => {
-			if (err) {
-				throw err;
-			}
-			resolve(rows);
+			if (err) return null;
+			resolve({
+				rows: rows.map(r => FIELDS.map(f => r[f])),
+				fields: FIELDS
+			});
 		});
 	});
 }
