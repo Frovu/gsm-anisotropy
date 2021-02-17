daacbb27c0083bc9bb40dbf1de4154d6877fe4a9
Frovu <sfrovis@gmail.com>
2021-02-17 15:37:08 +0300

    fix: hanging promise

--- modules/api.js
+++ modules/api.js
@@ -6,15 +6,14 @@ const db = new sqlite3.Database('gsm.db', sqlite3.OPEN_READONLY);
 const FIELDS = ['time', 'A0', 'Ax', 'Ay', 'Az', 'Axy'];
 
 function select(params) {
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		const from = params.from && parseInt(params.from);
 		const to = params.to && parseInt(params.to);
 		if(!from || isNaN(from) || !to || isNaN(to))
-			return null;
-		// cursed code here, golang devs would quite dislike it
+			return resolve(null);
 		const q = `SELECT ${FIELDS.join()} FROM gsm WHERE time >= ${from} AND time < ${to} ORDER BY time`;
 		db.all(q, [], (err, rows) => {
-			if (err) return null;
+			if (err) return reject(err);
 			resolve({
 				rows: rows.map(r => FIELDS.map(f => r[f])),
 				fields: FIELDS
@@ -24,11 +23,16 @@ function select(params) {
 }
 
 router.get('/data', async (req, res) => {
-	const resp = await select(req.query);
-	if(resp)
-		res.status(200).json(resp);
-	else
-		res.sendStatus(400);
+	try {
+		const resp = await select(req.query);
+		if(resp)
+			res.status(200).json(resp);
+		else
+			res.sendStatus(400);
+	} catch(e) {
+		global.log(e);
+		res.sendStatus(500);
+	}
 });
 
 module.exports = router;
